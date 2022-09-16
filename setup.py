8988841fc0370791c96c57140684a1e4dc512c5d
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-09-15 21:26:30 -0400

    [setup.py] Update to latest Python 2&3 compatible format

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
-from setuptools import setup
-from itertools import imap, ifilter
 from ast import parse
+from itertools import ifilter, imap
 
-if __name__ == '__main__':
-    package_name = 'vagrant2json'
+from setuptools import setup
 
-    with open(package_name + '.py') as f:
+if __name__ == "__main__":
+    package_name = "vagrant2json"
+
+    with open(package_name + ".py") as f:
         __author__, __version__ = imap(
             lambda buf: next(imap(lambda e: e.value.s, parse(buf).body)),
-            ifilter(lambda line: line.startswith('__version__') or line.startswith('__author__'), f)
+            ifilter(
+                lambda line: line.startswith("__version__")
+                or line.startswith("__author__"),
+                f,
+            ),
         )
 
     setup(
         name=package_name,
         author=__author__,
         version=__version__,
-        test_suite='tests',
-        py_modules=[package_name]
+        test_suite="tests",
+        py_modules=[package_name],
     )
